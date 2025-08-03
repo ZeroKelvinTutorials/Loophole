7bee9cb5c3881f7882653c6c16049944c73c5d7b
ZeroKelvinTutorials <96324255+ZeroKelvinTutorials@users.noreply.github.com>
2025-08-03 01:45:48 -0600

    Changed text on last level won message

--- Loophole/Assets/src/Buttons/LevelWonButton.cs
+++ Loophole/Assets/src/Buttons/LevelWonButton.cs
@@ -1,12 +1,34 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using view;
 public class LevelWonButton : MonoBehaviour
 {
+    public Text text;
     public void Press()
     {
         LevelController.DestroyActiveLevelView();
         PlayerInput.canTakeInput = true;
-        GameController.OpenLevel(GameManager.currentLevelNumber + 1);
+        if (GameManager.currentLevelNumber == LevelManager.Instance.levels.Length)
+        {
+            GameController.OpenLevel(1);
+        }
+        else
+        {
+            GameController.OpenLevel(GameManager.currentLevelNumber + 1);
+        }
         this.gameObject.SetActive(false);
+    }
+
+    void OnEnable()
+    {
+        if (GameManager.currentLevelNumber == LevelManager.Instance.levels.Length)
+        {
+            text.text = "You made it to the end, Thankyou!";
+        }
+        else
+        {
+            text.text = "Next Level";
+        }
     }
 }
